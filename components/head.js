30be5a318b05836344c6645b49769bf2f497fdd3
Duc Tho Tran <ducthotran2010@gmail.com>
2018-03-06 16:31:09 +0700

    fix head tag - link logo

--- components/head.js
+++ components/head.js
@@ -5,7 +5,7 @@ const Head = (props) => (
    <NextHead>
       <meta charset="UTF-8" />
       <title>{props.title || ''}</title>
-      <link rel="icon" href="../static/logo_recaptcha.png"/>
+      <link rel="icon" href="reCAPTCHA-nextjs/static/logo_recaptcha.png"/>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
    </NextHead>
 )
